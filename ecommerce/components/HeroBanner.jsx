22aff3f449de37407d25107c92fdb96127ca2675
Katherine Jiang <katherine.jiang30@gmail.com>
2022-07-08 16:43:28 -0400

    updated HeroBanner component

--- ecommerce/components/HeroBanner.jsx
+++ ecommerce/components/HeroBanner.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <div className="hero-banner-container">
+      <div>
+        {/* need to rename class here, not doing beats */}
+        <p className='beats-solo'>SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+        <img src="" alt="candles" className="hero-banner-image"></img>
+
+        <div>
+          <Link href="/product/ID">
+            <button type="button">BUTTON TEXT</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>DESCRIPTION</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
